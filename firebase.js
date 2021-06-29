b7a4220a4e7ae634017adee95e3fe7e96169be14
Kapil Bhise <bhisekapil38@gmail.com>
2021-06-29 14:21:53 +0530

    hide firebase credentials from repository

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYGPPDPGZcAZB-BnFP2VOm2mUU6-kTuL4",
-  authDomain: "awesome-signal.firebaseapp.com",
-  projectId: "awesome-signal",
-  storageBucket: "awesome-signal.appspot.com",
-  messagingSenderId: "1068858625141",
-  appId: "1:1068858625141:web:4006d106beb3a74f67bc3c",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 let app;
